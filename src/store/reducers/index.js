7f0353d8745651eb89e0b2aab0443e0fb4d39a15
Daniduf2001 <danidufernando2002@gmail.com>
2023-03-06 13:48:18 +0530

    combine all resucers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import userReducer from "./user.reducer";
+import studentReducer from "./student.reducer";
 
 export default combineReducers({
-    userReducer
+    userReducer,
+    studentReducer
 });
