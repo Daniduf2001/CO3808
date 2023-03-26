d98789cb4a6d7125dafa602c96dbb18bf6324b22
Daniduf2001 <danidufernando2002@gmail.com>
2023-03-26 19:12:57 +0530

    create logout function

--- src/components/admin/common/header/Header.js
+++ src/components/admin/common/header/Header.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import Cookies from "universal-cookie";
 import './header.css';
 // import logo from '../../../../assets/logo/logo.svg';
 import {Link} from "react-router-dom";
 
 const Header = () => {
+    const logout = () => {
+        const token = new Cookies()
+        token.remove('token');
+        window.location = "/";
+    }
+
     return (
         <div className="top_navbar">
             <div className="hamburger">
@@ -18,8 +25,8 @@ const Header = () => {
                 <div className="col-5 d-flex justify-content-end">
                     <ul className="mb-0">
                         <li className="mt-1 text-decoration-none">
-                            <p className="d-inline me-2"><i className="fa fa-sign-out logoutIcon" aria-hidden="true"/>
-                            </p>
+                            <p className="d-inline me-2" onClick={logout}><i className="fa fa-sign-out logoutIcon"
+                                                                             aria-hidden="true"/></p>
                             <p className="d-inline ms-2 me-2 fw-bold profileText">Madhusha</p>
                         </li>
                         <li>
